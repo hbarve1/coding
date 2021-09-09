42c7690047abb0dcf20d19cc8f86c54f669931a6
Himank Honey Barve <hbarve1592@gmail.com>
2021-09-09 22:22:29 +0530

    isNumberFibbonaci

--- src/pesto/isNumberFibbonaci.ts
+++ src/pesto/isNumberFibbonaci.ts
@@ -2,11 +2,7 @@
 
 // Check is given number is fibbonnaci number.
 
-function isPerfectSquare(x: number): boolean {
-  const s: number = parseInt(Math.sqrt(x).toString(), 10);
-
-  return s * s === x;
-}
+import isPerfectSquare from "./isPerfectSquare";
 
 function isNumberFibbonaci(n: number): boolean {
   return isPerfectSquare(5 * n * n + 4) || isPerfectSquare(5 * n * n - 4);
