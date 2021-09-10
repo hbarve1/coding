0529dc3fae5f2d2630095dd9a9bc0bbd76e5f90a
Himank Honey Barve <hbarve1592@gmail.com>
2021-09-10 18:07:12 +0530

    reversestring

---
+++ src/the-coding-interview-bootcamp/reversestring.test.ts
@@ -0,0 +1,15 @@
+/** @format */
+
+import reverse from "./reversestring";
+
+test("Reverse function exists", () => {
+  expect(reverse).toBeDefined();
+});
+
+test("Reverse reverses a string", () => {
+  expect(reverse("abcd")).toEqual("dcba");
+});
+
+test("Reverse reverses a string", () => {
+  expect(reverse("  abcd")).toEqual("dcba  ");
+});
---
+++ src/the-coding-interview-bootcamp/reversestring.ts
@@ -0,0 +1,15 @@
+/** @format */
+
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str: string): string {
+  return str.split("").reverse().join("");
+}
+
+export default reverse;
