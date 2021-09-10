acfdf3d4385228000843c42c1c5090bfc62ce06b
Himank Honey Barve <hbarve1592@gmail.com>
2021-09-10 18:07:58 +0530

    reverseInt

---
+++ src/the-coding-interview-bootcamp/reverseint.ts
@@ -0,0 +1,17 @@
+/** @format */
+
+// --- Directions
+// Given an integer, return an integer that is the reverse
+// ordering of numbers.
+// --- Examples
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+//   reverseInt(500) === 5
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
+
+function reverseInt(n: number): number {
+  return parseInt(String(n).split("").reverse().join(""), 10) * Math.sign(n);
+}
+
+export default reverseInt;
