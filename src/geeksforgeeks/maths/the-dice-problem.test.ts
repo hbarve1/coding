b703df22648ad8a9e4f4c19deb981316b134fb53
Himank Honey Barve <hbarve1592@gmail.com>
2021-09-11 09:51:29 +0530

    theDiceProblem

---
+++ src/geeksforgeeks/maths/the-dice-problem.test.ts
@@ -0,0 +1,11 @@
+/** @format */
+
+import theDiceProblem from "./the-dice-problem";
+
+describe("", () => {
+  test("", () => {
+    expect(theDiceProblem(1)).toBe(6);
+    expect(theDiceProblem(2)).toBe(5);
+    expect(theDiceProblem(3)).toBe(4);
+  });
+});
---
+++ src/geeksforgeeks/maths/the-dice-problem.ts
@@ -0,0 +1,8 @@
+/** @format */
+// https://practice.geeksforgeeks.org/problems/the-dice-problem2316/1/?category[]=Mathematical&category[]=Mathematical&page=1&query=category[]Mathematicalpage1category[]Mathematical
+
+function theDiceProblem(n: number): number {
+  return 7 - n;
+}
+
+export default theDiceProblem;
