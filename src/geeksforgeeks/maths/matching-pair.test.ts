999e2bb98641bf0d0f508ab9019849fc9fd5200e
Himank Honey Barve <hbarve1592@gmail.com>
2021-09-11 09:50:06 +0530

    matchingPair

---
+++ src/geeksforgeeks/maths/matching-pair.test.ts
@@ -0,0 +1,12 @@
+/** @format */
+
+import matchingPair from "./matching-pair";
+
+describe("", () => {
+  test("", () => {
+    expect(matchingPair(1)).toBe(2);
+    expect(matchingPair(10)).toBe(11);
+    expect(matchingPair(8)).toBe(9);
+    expect(matchingPair(2)).toBe(3);
+  });
+});
---
+++ src/geeksforgeeks/maths/matching-pair.ts
@@ -0,0 +1,8 @@
+/** @format */
+// https://practice.geeksforgeeks.org/problems/matching-pair5320/1/?category[]=Mathematical&category[]=Mathematical&page=1&query=category[]Mathematicalpage1category[]Mathematical#
+
+function matchingPair(n: number): number {
+  return n + 1;
+}
+
+export default matchingPair;
