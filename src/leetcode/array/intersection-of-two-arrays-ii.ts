c61ea387e3188cf224378be8f748e4c6115ee642
Himank Barve <hbarve1592@gmail.com>
2021-09-20 15:54:29 +0530

    Intersection of Two Arrays II

---
+++ src/leetcode/array/intersection-of-two-arrays-ii.ts
@@ -0,0 +1,37 @@
+/** @format */
+
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/646/
+
+/* eslint-disable no-plusplus */
+/* eslint-disable no-param-reassign */
+
+function reduceFn(arr: number[] = [], val: number) {
+  if (arr[val]) arr[val] += 1;
+  else arr[val] = 1;
+
+  return arr;
+}
+
+function intersect(nums1: number[], nums2: number[]): number[] {
+  const arr1 = nums1.reduce(reduceFn, []);
+  const arr2 = nums2.reduce(reduceFn, []);
+
+  const results = [];
+
+  for (let i = 0; i < Math.min(arr1.length, arr2.length); i += 1) {
+    const val1 = arr1[i];
+    const val2 = arr2[i];
+
+    if (typeof val1 !== "undefined" && typeof val2 !== "undefined") {
+      results.push(
+        ...(Array.from({ length: Math.min(val1, val2) }).fill(i) as number[]),
+      );
+    }
+  }
+
+  // console.log(results);
+
+  return results;
+}
+
+export default intersect;
